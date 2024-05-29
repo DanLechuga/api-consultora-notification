56181ed14e9da556515543bfff89fad72b2ca17a
danmc400live <danlechuga@live.com>
2024-05-28 21:52:20 -0600

    add logic notification

--- notification_Service/notificationService.py
+++ notification_Service/notificationService.py
@@ -1,9 +1,25 @@
 from notification_middleware.dto.request import sendNotification
+import smtplib
+import os
+from email.message import EmailMessage
+import ssl
 
+def getVariable(name):
+    return os.environ.get(name,"NOT FOUND")
 
 
 async def sendMail(notification : sendNotification.sendNotification):
+    destinatario = getVariable("user")
+    asunto = "Contacto para cotizacion de proyecto"
+    emailMessages = EmailMessage()
+    emailMessages['From'] = notification.email
+    emailMessages['To'] = destinatario
+    emailMessages['Subject'] = asunto
+    msj = f"Posible cliente con identificaciones \n email:{notification.email},\n numero de telefono: {notification.phoneNumber} \n envio el siguiente mensaje "
+    msj = msj + str(notification.message)
+    emailMessages.set_content(msj)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com",465,context= context) as smtp:
+        smtp.login(destinatario,getVariable("keyGmail"))
+        smtp.sendmail(str(notification.email),destinatario,emailMessages.as_string())
     
-    
-    
-    return ""
